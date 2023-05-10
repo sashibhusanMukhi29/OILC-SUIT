667ab2e1f4b000762f0776aaa039d03b23a4bca5
JashwanthPendyala <jashwanth.pendyala@ojas-it.com>
2023-05-10 15:29:29 +0530

    initial commit

---
+++ src/main/java/com/ojas/oilcsuite/admin/OilcSuiteAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ojas.oilcsuite.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OilcSuiteAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OilcSuiteAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ojas/oilcsuite/admin/OilcSuiteAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojas.oilcsuite.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OilcSuiteAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
