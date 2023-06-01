0e3e4d42b4ed86366203cd173d58bd62b1669512
D3 <sashibhusanmukhi@gmail.com>
2023-06-01 19:09:04 +0530

    config server

---
+++ src/test/java/com/oilc_suit/config_server/OilcSuitConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oilc_suit.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OilcSuitConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
